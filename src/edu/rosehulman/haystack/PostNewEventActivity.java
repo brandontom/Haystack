754edbde2bdd7a317fdaa1ca35ebb78356eaa2b0
songm <songm@rose-hulman.edu>
2015-01-25 18:59:18 -0500

    process of adding postnewactivity

---
+++ src/edu/rosehulman/haystack/PostNewEventActivity.java
@@ -0,0 +1,15 @@
+package edu.rosehulman.haystack;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PostNewEventActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_post_new_event);
+	}
+}
