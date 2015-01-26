517d8879f6cc0bd2694e705ded63f16e6bcbf8fd
songm <songm@rose-hulman.edu>
2015-01-26 15:39:27 -0500

    added new event post activity design

--- src/edu/rosehulman/haystack/MainActivity.java
+++ src/edu/rosehulman/haystack/MainActivity.java
@@ -19,7 +19,6 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class MainActivity extends Activity implements
 		SideSwipeFragment.NavigationDrawerCallbacks {
