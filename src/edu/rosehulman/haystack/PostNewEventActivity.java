8f6cc1aaf521825dff3e7e457c1282651673f254
songm <songm@rose-hulman.edu>
2015-02-08 16:06:41 -0500

    finished up postneweventactivity

--- src/edu/rosehulman/haystack/PostNewEventActivity.java
+++ src/edu/rosehulman/haystack/PostNewEventActivity.java
@@ -1,15 +1,178 @@
 package edu.rosehulman.haystack;
 
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class PostNewEventActivity extends Activity {
+
+	EditText title;
+	Button fromTimePicker;
+	Button fromDatePicker;
+	Button toTimePicker;
+	Button toDatePicker;
+	EditText address;
+	EditText description;
+	Button postButton;
+	Button cancelButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_post_new_event);
+
+		title = (EditText) findViewById(R.id.edit_title);
+
+		fromTimePicker = (Button) findViewById(R.id.from_time_button);
+		fromTimePicker.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showTimePickerDialog();
+			}
+
+		});
+		// Calendar cal = Calendar.getInstance();
+		// cal.getTime();
+		// SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+		//
+		//
+		// fromTimePicker.setText(sdf.format(cal.getTime()));
+
+		fromDatePicker = (Button) findViewById(R.id.from_date_button);
+		fromDatePicker.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showDatePickerDialog();
+			}
+
+		});
+
+		toTimePicker = (Button) findViewById(R.id.to_time_button);
+		toTimePicker.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showTimePickerDialog();
+			}
+
+		});
+
+		toDatePicker = (Button) findViewById(R.id.to_date_button);
+		toDatePicker.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showDatePickerDialog();
+			}
+
+		});
+
+		address = (EditText) findViewById(R.id.edit_address);
+		description = (EditText) findViewById(R.id.edit_description);
+		
+		postButton = (Button) findViewById(R.id.post_button);
+		postButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		cancelButton = (Button) findViewById(R.id.cancel_button);
+		cancelButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+
+	public static class TimePickerFragment extends DialogFragment implements
+			TimePickerDialog.OnTimeSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current time as the default values for the picker
+			final Calendar c = Calendar.getInstance();
+			int hour = c.get(Calendar.HOUR_OF_DAY);
+			int minute = c.get(Calendar.MINUTE);
+
+			// Create a new instance of TimePickerDialog and return it
+			return new TimePickerDialog(getActivity(), this, hour, minute,
+					DateFormat.is24HourFormat(getActivity()));
+		}
+
+		public void onTimeSet(TimePicker view, final int hourOfDay, final int minute) {
+			// Do something with the time chosen by the user
+
+			Context context = super.getActivity();
+			CharSequence text = "Current time is " + hourOfDay + ":" + minute;
+			int duration = Toast.LENGTH_SHORT;
+
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+		}
+	}
+
+	private void showTimePickerDialog() {
+		DialogFragment df = new TimePickerFragment();
+
+		df.show(getFragmentManager(), "");
+
+	}
+
+	public static class DatePickerFragment extends DialogFragment implements
+			DatePickerDialog.OnDateSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current date as the default date in the picker
+			final Calendar c = Calendar.getInstance();
+			int year = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH);
+			int day = c.get(Calendar.DAY_OF_MONTH);
+
+			// Create a new instance of DatePickerDialog and return it
+			return new DatePickerDialog(getActivity(), this, year, month, day);
+		}
+
+		public void onDateSet(DatePicker view, int year, int month, int day) {
+			// Do something with the date chosen by the user
+			Context context = super.getActivity();
+			DateFormatSymbols date = new DateFormatSymbols();
+			CharSequence text = "Current date is " + date.getMonths()[month] + " " + day + ", "
+					+ year;
+			int duration = Toast.LENGTH_SHORT;
+
+			Toast toast = Toast.makeText(context, text, duration);
+			toast.show();
+		}
+	}
+
+	private void showDatePickerDialog() {
+		// TODO Auto-generated method stub
+		DialogFragment df = new DatePickerFragment();
+		df.show(getFragmentManager(), "");
 	}
 }
